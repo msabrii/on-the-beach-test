62e54af875a5b57f61188c35dc3e61cd162c75ed
msabrii <ameer.sabri@hotmail.co.uk>
2025-02-12 17:04:36 +0000

    feat: create sort options component

---
+++ src/app/components/SortOptions/SortOptions.tsx
@@ -0,0 +1,50 @@
+import cn from "@/utils/cn";
+import React from "react";
+
+const SortOptions = ({
+  currentSortOption,
+  setSortOption,
+}: {
+  currentSortOption: string;
+  setSortOption: (option: string) => void;
+}) => {
+  return (
+    <div className="flex flex-col col-span-12 sm:col-span-4 lg:col-span-3">
+      <button
+        type="button"
+        className={cn("px-4 py-2 bg-white text-left text-dark-blue", {
+          "bg-dark-blue text-white": currentSortOption === "alphabetic",
+        })}
+        onClick={() => {
+          setSortOption("alphabetic");
+        }}
+      >
+        sort <span className="font-semibold">alphabetically</span>
+      </button>
+      <button
+        type="button"
+        className={cn("px-4 py-2 bg-white text-left text-dark-blue", {
+          "bg-dark-blue text-white": currentSortOption === "price",
+        })}
+        onClick={() => {
+          setSortOption("price");
+        }}
+      >
+        sort by <span className="font-semibold">price</span>
+      </button>
+      <button
+        type="button"
+        className={cn("px-4 py-2 bg-white text-left text-dark-blue", {
+          "bg-dark-blue text-white": currentSortOption === "star",
+        })}
+        onClick={() => {
+          setSortOption("star");
+        }}
+      >
+        sort by <span className="font-semibold">star rating</span>
+      </button>
+    </div>
+  );
+};
+
+export default SortOptions;
